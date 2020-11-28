293eb736dd7b544865f59754706064c7a0ad5444
Mustafakonya <av.mustafakonya@gmail.com>
2020-11-28 20:04:11 +0100

    featureden commit

--- src/git_project_branch2/BranchDeneme.java
+++ src/git_project_branch2/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 		
 		System.out.println("16.28 master branch den ilk commit");
+		System.out.println("18.36 feature branch den ilk commit");
 	}
 
 }
