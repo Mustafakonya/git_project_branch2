02ff8cab593d4b3cc49273dbd23a09ebea6cd626
Mustafakonya <av.mustafakonya@gmail.com>
2020-11-28 19:57:26 +0100

    first commit from master

--- src/git_project_branch2/BranchDeneme.java
+++ src/git_project_branch2/BranchDeneme.java
@@ -3,8 +3,8 @@ package git_project_branch2;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("16.28 master branch den ilk commit");
 	}
 
 }
